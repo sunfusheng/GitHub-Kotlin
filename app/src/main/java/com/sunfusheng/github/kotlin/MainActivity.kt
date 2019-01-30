85f7a2cee525d8789b3234b561b6ac9854845053
sunfusheng <sfsheng0322@gmail.com>
2019-01-30 18:09:29 +0800

    test svg vector

--- app/src/main/java/com/sunfusheng/github/kotlin/MainActivity.kt
+++ app/src/main/java/com/sunfusheng/github/kotlin/MainActivity.kt
@@ -2,30 +2,25 @@ package com.sunfusheng.github.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var user: User
-    @Inject
-    lateinit var repo: Repo
+    var flag: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userName = "sunfusheng"
-        val repoName = "MarqueeView"
+        vAccount.setOnClickListener {
+            if (!flag) {
+                vAccount.setImageResource(R.drawable.ic_account)
+            } else {
+                vAccount.setImageResource(R.drawable.ic_account_outline)
+            }
+            flag = !flag
+        }
 
-        DaggerMainComponent.builder()
-            .userModule(UserModule(userName))
-            .repoModule(RepoModule(repoName))
-            .build()
-            .inject(this);
-
-        user.info()
-        repo.info()
     }
 
 }
