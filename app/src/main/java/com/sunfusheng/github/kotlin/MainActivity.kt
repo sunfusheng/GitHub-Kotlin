d5612c961fa80ffa9813d960d5893f2f3a924dfe
sunfusheng <sfsheng0322@gmail.com>
2019-01-31 20:02:07 +0800

    test svg vector

--- app/src/main/java/com/sunfusheng/github/kotlin/MainActivity.kt
+++ app/src/main/java/com/sunfusheng/github/kotlin/MainActivity.kt
@@ -2,24 +2,12 @@ package com.sunfusheng.github.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var flag: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        vAccount.setOnClickListener {
-            if (!flag) {
-                vAccount.setImageResource(R.drawable.ic_account)
-            } else {
-                vAccount.setImageResource(R.drawable.ic_account_outline)
-            }
-            flag = !flag
-        }
 
     }
 
