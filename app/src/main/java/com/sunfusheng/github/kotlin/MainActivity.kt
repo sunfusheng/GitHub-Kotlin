10547d5d8c9cbfb9785694460d4e79718e4877f4
sunfusheng <sfsheng0322@gmail.com>
2019-05-24 14:45:17 +0800

    init

--- app/src/main/java/com/sunfusheng/github/kotlin/MainActivity.kt
+++ app/src/main/java/com/sunfusheng/github/kotlin/MainActivity.kt
@@ -2,13 +2,24 @@ package com.sunfusheng.github.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.sunfusheng.github.kotlin.util.StatusBarUtil
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StatusBarUtil.setTranslucent(this)
         setContentView(R.layout.activity_main)
 
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val layoutParams = vStatusBar.layoutParams
+        layoutParams.height = StatusBarUtil.getStatusBarHeight(this)
+        vStatusBar.layoutParams = layoutParams
+        vToolbar.title = getString(R.string.app_name)
     }
 
 }
